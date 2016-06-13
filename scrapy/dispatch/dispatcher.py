e5face5f134f2bbf59cfa43138da64b420fca71e
rootavish <rootavish@gmail.com>
2016-06-13 07:20:48 +0530

    Added: A send_robust modification to handle twisted deferreds

--- scrapy/dispatch/dispatcher.py
+++ scrapy/dispatch/dispatcher.py
@@ -2,11 +2,12 @@ import sys
 import threading
 import warnings
 import weakref
-
+import logging
 import six
 from scrapy.exceptions import ScrapyDeprecationWarning
 from utils.inspect import func_accepts_kwargs
 from six.moves import range
+from twisted.internet.defer import maybeDeferred, DeferredList, Deferred
 
 if six.PY2:
     from .weakref_backports import WeakMethod
@@ -22,6 +23,8 @@ NONE_ID = _make_id(None)
 
 # A marker for caching
 NO_RECEIVERS = object()
+
+logger = logging.getLogger(__name__)
 
 
 class Signal(object):
@@ -98,8 +101,8 @@ class Signal(object):
 
         # Check for **kwargs
         if not func_accepts_kwargs(receiver):
-                raise ValueError(
-                 "Signal receivers must accept keyword arguments (**kwargs).")
+            raise ValueError(
+                "Signal receivers must accept keyword arguments (**kwargs).")
 
         if dispatch_uid:
             lookup_key = (dispatch_uid, _make_id(sender))
@@ -237,6 +240,53 @@ class Signal(object):
             else:
                 responses.append((receiver, response))
         return responses
+
+    def send_robust_deferred(self, sender, **named):
+        """
+        Send signal from sender to all connected receivers catching errors.
+        Modified to work with functions returning twisted deferreds.
+        Arguments:
+
+            sender
+                The sender of the signal. Can be any python object (normally one
+                registered with a connect if you actually want something to
+                occur).
+
+            named
+                Named arguments which will be passed to receivers. These
+                arguments must be a subset of the argument names defined in
+                providing_args.
+
+        Return a list of tuple pairs [(receiver, response), ... ]. May raise
+        DispatcherKeyError.
+
+        If any receiver raises an error (specifically any subclass of
+        Exception), the error instance is returned as the result for that
+        receiver. The traceback is always attached to the error at
+        ``__traceback__``.
+        """
+        dfds = []
+        if not self.receivers or self.sender_receivers_cache.get(sender) is NO_RECEIVERS:
+            return dfds
+
+        # Call each receiver with whatever arguments it can accept.
+        # Return a list of tuple pairs [(receiver, response), ... ].
+        for receiver in self._live_receivers(sender):
+            try:
+                dfd = maybeDeferred(
+                    receiver,  signal=self, sender=sender, **named)
+                dfd.addErrback(logerror, receiver)
+                dfd.addBoth(lambda result: (receiver, result))
+                dfds.append(dfd)
+            except Exception as err:
+                if not hasattr(err, '__traceback__'):
+                    err.__traceback__ = sys.exc_info()[2]
+                responses.append((receiver, err))
+            else:
+                responses.append((receiver, response))
+        d = DeferredList(dfds)
+        d.addCallback(lambda out: [x[1] for x in out])
+        return d
 
     def _clear_dead_receivers(self):
         # Note: caller is assumed to hold self.lock.
