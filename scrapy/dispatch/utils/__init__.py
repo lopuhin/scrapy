b9d69009026034eb8148f4fb880a5ae706fc808f
rootavish <rootavish@gmail.com>
2016-06-26 15:34:27 +0530

    Prototype support for handlers that don't accept keyword arguments

--- scrapy/dispatch/utils/__init__.py
+++ scrapy/dispatch/utils/__init__.py
@@ -1,3 +1,5 @@
 """This module is used to port the utils required by the django signals API over
 from django.utils. Originally written by the Django project.
 """
+
+from inspect import func_accepts_kwargs
