754d93991c3ed6cfe02605f9a0e72f78a048e38c
Avishkar Gupta <rootavish@gmail.com>
2016-08-15 20:54:15 +0530

    Use same code in robust_apply under Python 2 and 3, use magic string concatenation

--- scrapy/dispatch/utils/robustapply.py
+++ scrapy/dispatch/utils/robustapply.py
@@ -10,11 +10,9 @@ import six
 
 def function(receiver):
     """Get function-like callable object for given receiver
-
-    returns (function_or_method, codeObject, fromMethod)
-
+    returns (function_or_method, codeObject, fromMethod).
     If fromMethod is true, then the callable already
-    has its first argument bound
+    has its first argument bound.
     """
     if hasattr(receiver, '__call__'):
         # receiver is a class instance; assume it is callable.
@@ -24,11 +22,11 @@ def function(receiver):
             receiver = receiver.__call__
     if hasattr(receiver, '__func__'):
         # an instance-method...
-        return receiver, receiver.__code__, 1
+        return receiver, receiver.__code__, True
     elif not hasattr(receiver, '__code__'):
         raise ValueError('unknown reciever type %s %s' %
                          (receiver, type(receiver)))
-    return receiver, receiver.__code__, 0
+    return receiver, receiver.__code__, False
 
 
 def robust_apply(receiver, *arguments, **named):
@@ -40,20 +38,15 @@ def robust_apply(receiver, *arguments, **named):
     for name in codeObject.co_varnames[startIndex:startIndex+len(arguments)]:
         if name in named:
             raise TypeError(
-                """Argument %r specified both positionally and
-                as a keyword for calling %r""" % (
+                """Argument %r specified both positionally and"""
+                """as a keyword for calling %r""" % (
                     name, receiver,
                 )
             )
     if not (codeObject.co_flags & 8):
         # fc does not have a **kwds type parameter, therefore
         # remove unacceptable arguments.
-        if six.PY2:
-            for arg in named.keys():
-                if arg not in acceptable:
-                    del named[arg]
-        else:
-            for arg in list(named):
-                if arg not in acceptable:
-                    del named[arg]
+        for arg in list(named):
+            if arg not in acceptable:
+                del named[arg]
     return receiver(*arguments, **named)
